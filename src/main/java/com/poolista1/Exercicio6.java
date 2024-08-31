e90864b68452e8dfd33d8e74febd43d99cfd2baf
daianefersan <dra.daianeferreira@gmail.com>
2024-08-30 22:36:06 -0300

    Exercício6

---
+++ src/main/java/com/poolista1/Exercicio6.java
@@ -0,0 +1,26 @@
+package com.poolista1;
+
+import java.util.Scanner;
+
+public class Exercicio6 {
+    public static void resolucao(){
+        Scanner leia = new Scanner(System.in);
+
+        int distanciakm;
+        int combustivelGasto;
+        int consumoMedio;
+
+        System.out.println("Digite a distância percorrida em km: ");
+        distanciakm = leia.nextInt();
+
+        System.out.println("Digite o total de combustível gasto: ");
+        combustivelGasto = leia.nextInt();
+
+        consumoMedio = distanciakm/combustivelGasto;
+
+        System.out.println("O consumo médio do veículo é: "+consumoMedio+"km/l");
+
+
+    }
+
+}
--- src/main/java/com/poolista1/Poolista1Application.java
+++ src/main/java/com/poolista1/Poolista1Application.java
@@ -9,7 +9,7 @@ public class Poolista1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Poolista1Application.class, args);
 
-		Exercicio5.resolucao();
+		Exercicio6.resolucao();
 		
 	}
 
