9c3713ae59caf504ac304258accc7643f0fa5444
daianefersan <dra.daianeferreira@gmail.com>
2024-09-02 18:40:13 -0300

    EXERCICIO8 LISTA2

--- src/main/java/com/poolista1/principal/Poolista2Application.java
+++ src/main/java/com/poolista1/principal/Poolista2Application.java
@@ -3,6 +3,7 @@ package com.poolista1.principal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.poolista1.principal.listamatematica.Exercicio8;
 import com.poolista1.principal.listamatematica2.Exercicio1;
 import com.poolista1.principal.listamatematica2.Exercicio2;
 import com.poolista1.principal.listamatematica2.Exercicio3;
@@ -17,7 +18,7 @@ public class Poolista2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Poolista1Application.class, args);
 
-		Exercicio7.resolucao();
+		Exercicio8.resolucao();
 		
 	}
 
---
+++ src/main/java/com/poolista1/principal/listamatematica2/Exercicio8.java
@@ -0,0 +1,59 @@
+package com.poolista1.principal.listamatematica2;
+import java.util.Scanner;
+
+public class Exercicio8 {
+
+    public static void resolucao(){
+
+        /*Crie um joguinho de perguntas e respostas múltipla escolha. O programa
+deverá fazer 5 perguntas (Uma por vez). Se ele errar 3 vezes ele perde o jogo. Se
+o usuário chegar até o final o programa deve exibir o número de acertos. */
+
+        Scanner leia = new Scanner(System.in);
+
+        int erros = 0; 
+        int acertos = 0; 
+
+        
+        String[] perguntas = {
+            "1. Qual é a capital da França?\n(a) Londres\n(b) Berlim\n(c) Paris\n(d) Madrid",
+            "2. Quanto é 2 + 2?\n(a) 3\n(b) 4\n(c) 5\n(d) 6",
+            "3. Qual é o maior planeta do sistema solar?\n(a) Terra\n(b) Marte\n(c) Júpiter\n(d) Saturno",
+            "4. Quem pintou a Mona Lisa?\n(a) Van Gogh\n(b) Leonardo da Vinci\n(c) Picasso\n(d) Monet",
+            "5. Qual é o elemento químico representado pelo símbolo 'O'?\n(a) Ouro\n(b) Oxigênio\n(c) Osmio\n(d) Prata"
+        };
+
+
+        char[] respostasCorretas = {'c', 'b', 'c', 'b', 'b'};
+
+        // Itera sobre as perguntas
+        for (int i = 0; i < perguntas.length; i++) {
+            System.out.println(perguntas[i]);
+            System.out.print("Sua resposta: ");
+            char resposta = leia.next().toLowerCase().charAt(0);
+
+            // Verifica se a resposta está correta
+            if (resposta == respostasCorretas[i]) {
+                System.out.println("Resposta correta!\n");
+                acertos++;
+            } else {
+                System.out.println("Resposta incorreta.\n");
+                erros++;
+            }
+
+            if (erros == 3) {
+                System.out.println("Você errou 3 vezes. Fim de jogo!");
+                break;
+            }
+        }
+
+        if (erros < 3) {
+            System.out.println("Você terminou o jogo com " + acertos + " acertos!");
+        }
+
+        leia.close();
+
+    }
+
+
+}
