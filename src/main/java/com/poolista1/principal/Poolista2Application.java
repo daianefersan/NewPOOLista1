522245152f13797451b590e1e388f0af69b505ff
daianefersan <dra.daianeferreira@gmail.com>
2024-09-02 14:41:04 -0300

    EXERCICIO2 LISTA2

--- src/main/java/com/poolista1/principal/Poolista2Application.java
+++ src/main/java/com/poolista1/principal/Poolista2Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.poolista1.principal.listamatematica2.Exercicio1;
+import com.poolista1.principal.listamatematica2.Exercicio2;
 
 @SpringBootApplication
 public class Poolista2Application {
@@ -11,7 +12,7 @@ public class Poolista2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Poolista1Application.class, args);
 
-		Exercicio1.resolucao();
+		Exercicio2.resolucao();
 		
 	}
 
---
+++ src/main/java/com/poolista1/principal/listamatematica2/Exercicio2.java
@@ -0,0 +1,29 @@
+package com.poolista1.principal.listamatematica2;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+    public static void resolucao(){
+
+        /*Desenvolva um algoritmo que seja capaz de receber dois números digitados pelo usuário e diga qual deles é maior.*/
+
+        Scanner leia = new Scanner(System.in);
+
+        int numero1 , numero2 ;
+
+        System.out.println("Digite o primeiro número");
+        numero1 = leia.nextInt();
+
+        System.out.println("Digite o segundo número");
+        numero2 = leia.nextInt();
+
+        if (numero1 >numero2) {
+            System.out.println("O N° maior é : "+ numero1);
+        } else {
+            System.out.println("O N° maior é : "+ numero2);
+        }
+
+        leia.close();
+    }
+}
