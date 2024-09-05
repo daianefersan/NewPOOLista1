ae64117f24f5f2b6f0662b228f01aa50628dca8f
daianefersan <dra.daianeferreira@gmail.com>
2024-09-05 08:50:57 -0300

    EXERCICIO6 LISTA3

--- src/main/java/com/poolistas/principal/Poolista3Application.java
+++ src/main/java/com/poolistas/principal/Poolista3Application.java
@@ -2,8 +2,10 @@ package com.poolistas.principal;
 
 import org.springframework.boot.SpringApplication;
 
+import com.poolistas.principal.listamatematica3.Exercicio01;
 import com.poolistas.principal.listamatematica3.Exercicio1;
 import com.poolistas.principal.listamatematica3.Exercicio4;
+import com.poolistas.principal.listamatematica3.Exercicio6;
 import com.poolistas.principal.listamatematica3.Exercicio7;
 
 public class Poolista3Application {
@@ -11,7 +13,7 @@ public class Poolista3Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Poolista3Application.class, args);
 
-		Exercicio7.resolucao();
+		Exercicio6.resolucao();
 		
 	}
 
