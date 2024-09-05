2ecf780b8c387df6f0a18a0c4f2fc24075ed6b18
daianefersan <dra.daianeferreira@gmail.com>
2024-09-05 09:47:49 -0300

    EXERCICIO1 LISTA3

--- src/main/java/com/poolistas/principal/Poolista3Application.java
+++ src/main/java/com/poolistas/principal/Poolista3Application.java
@@ -2,6 +2,7 @@ package com.poolistas.principal;
 
 import org.springframework.boot.SpringApplication;
 
+import com.poolistas.principal.listamatematica3.Exercicio1;
 import com.poolistas.principal.listamatematica3.Exercicio3;
 import com.poolistas.principal.listamatematica3.Exercicio4;
 import com.poolistas.principal.listamatematica3.Exercicio6;
@@ -13,7 +14,7 @@ public class Poolista3Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Poolista3Application.class, args);
 
-		Exercicio3.resolucao();
+		Exercicio1.resolucao();
 		
 	}
 
--- src/main/java/com/poolistas/principal/listamatematica3/Exercicio1.java
+++ src/main/java/com/poolistas/principal/listamatematica3/Exercicio1.java
@@ -1,1 +1,38 @@
+package com.poolistas.principal.listamatematica3;
 
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+    /*1) Escreva um programa que sirva como um cronômetro. O usuário deve
+        digitar um numero em segundos e o programa deve contabilizar os segundos
+        digitados. O programa deve receber somente números entre 1 e 59, do
+        contrário ele retornar um erro e solicitar que o usuário digite um número válido.*/
+
+    public static void resolucao(){
+
+        Scanner leia = new Scanner(System.in);
+
+        int numero;
+
+        System.out.print("Digite o tempo a ser cronometrado: ");
+        numero= leia.nextInt();
+
+        if((numero>= 60) || (numero <=0)){
+        System.out.print("Atenção!!! Digite um número entre 1 e 59 para iniciar seu cronômetro:  ");
+        }else{
+        for (int i =numero ; i > 0 ; i--) {
+        System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Tempo esgotado!");
+        }
+        
+        leia.close();
+        }
+    }
