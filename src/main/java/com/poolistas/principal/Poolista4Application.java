f419cd3b2ab17dcf373b5bc34bf1901cfee4f99c
daianefersan <dra.daianeferreira@gmail.com>
2024-09-05 23:44:45 -0300

    EXERCICIO3 LISTA4

--- src/main/java/com/poolistas/principal/Poolista4Application.java
+++ src/main/java/com/poolistas/principal/Poolista4Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.poolistas.principal.listamatematica4.Exercicio1;
 import com.poolistas.principal.listamatematica4.Exercicio2;
+import com.poolistas.principal.listamatematica4.Exercicio3;
 
 @SpringBootApplication
 public class Poolista4Application {
@@ -12,7 +13,7 @@ public class Poolista4Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Poolista4Application.class, args);
 
-		Exercicio2.resolucao();
+		Exercicio3.resolucao();
 		
 	}
 
--- src/main/java/com/poolistas/principal/listamatematica4/Exercicio3.java
+++ src/main/java/com/poolistas/principal/listamatematica4/Exercicio3.java
@@ -1,5 +1,37 @@
 package com.poolistas.principal.listamatematica4;
 
-public class Exercicio3 {
+import java.util.Scanner;
 
+public class Exercicio3 {
+    /*3) Ainda utilizando o exercício do mercado, calcule o ticket médio de
+        vendas do dia */
+    public static void resolucao() {
+
+        int nVendas;
+        double soma = 0;
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Bem-vindo ao sistema de vendas!");
+
+        System.out.println("Por favor digite quantas vendas foram realizadas hoje: ");
+        nVendas = leia.nextInt();
+
+        double[] vendas = new double[nVendas];
+
+        for (int i = 0 ; i < nVendas ; i++) {
+            System.out.print("Digite o valor da " + (i+1) + "° venda" + ": ");
+            vendas[i] = leia.nextDouble();
+            
+        }
+        for (double venda : vendas) {
+            soma += venda;    
+        }
+        System.out.print("\nO total das vendas foi: R$" + soma+ "\n\n");
+
+        double ticketMedio = (soma/nVendas);
+
+        System.out.println("O ticket médio de vendas do dia foi: R$"+ticketMedio);
+
+        System.out.println("Fechamento encerrado!");
+    }
 }
