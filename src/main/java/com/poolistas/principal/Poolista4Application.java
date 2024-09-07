422a939b1e6b8c2f98065cd1b96257e7c20c6b8c
daianefersan <dra.daianeferreira@gmail.com>
2024-09-07 19:29:41 -0300

    EXERCICIO8 LISTA4

--- src/main/java/com/poolistas/principal/Poolista4Application.java
+++ src/main/java/com/poolistas/principal/Poolista4Application.java
@@ -9,6 +9,7 @@ import com.poolistas.principal.listamatematica4.Exercicio4;
 import com.poolistas.principal.listamatematica4.Exercicio5;
 import com.poolistas.principal.listamatematica4.Exercicio6;
 import com.poolistas.principal.listamatematica4.Exercicio7;
+import com.poolistas.principal.listamatematica4.Exercicio8;
 
 @SpringBootApplication
 public class Poolista4Application {
@@ -17,7 +18,7 @@ public class Poolista4Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Poolista4Application.class, args);
 
-		Exercicio7.resolucao();
+		Exercicio8.resolucao();
 		
 	}
 
